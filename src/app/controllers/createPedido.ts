392b7237681f770383b3f7aa5b66f62b8b896c2a
byEduarda <albuquerque.zxl@gmail.com>
2025-07-08 19:45:25 -0300

    .

--- src/app/controllers/createPedido.ts
+++ src/app/controllers/createPedido.ts
@@ -2,13 +2,16 @@ import { Request, Response } from 'express';
 import Pedido from '../../core/models/Pedido';
 
 export async function createPedido(req: Request, res: Response) {
-  const { usuarioId, produto, quantidade } = req.body;
+  const { usuarioId, produto, quantidade, idempotenciaChave} = req.body;
 
   const pedido = await Pedido.create({
     usuarioId,
     produto,
-    quantidade
+    quantidade,
+    idempotenciaChave
   });
+
+
 
   return res.status(201).json(pedido);
 }
