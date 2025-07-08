80ab625c45fbd5882b15040465b9e539e7ed2ee0
byEduarda <albuquerque.zxl@gmail.com>
2025-07-08 20:01:11 -0300

    Update createPedido.ts

--- src/app/controllers/createPedido.ts
+++ src/app/controllers/createPedido.ts
@@ -4,13 +4,22 @@ import Pedido from '../../core/models/Pedido';
 export async function createPedido(req: Request, res: Response) {
   const { usuarioId, produto, quantidade, idempotenciaChave} = req.body;
 
-  const pedido = await Pedido.create({
+  
+if(!idempotenciaChave){
+  return res.status(400).json({message: 'A idempotenciaChave é obrigatória'})
+}
+
+const existe = await Pedido.findOne({idempotenciaChave});
+if(existe){
+  return res.status(400).json({message: 'Já cadastrado!'});
+}
+
+const pedido = await Pedido.create({
     usuarioId,
     produto,
     quantidade,
     idempotenciaChave
   });
-
 
 
   return res.status(201).json(pedido);
